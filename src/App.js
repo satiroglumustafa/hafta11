ce4f3d2c91713ec1357d552eabfb848280f902f5
satiroglumustafa <mmustafa.satiroglu@gmail.com>
2024-08-11 16:19:42 +0300

    Bootsrapt paketi projeye eklendi

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col">
+          <ol className="list-group list-group-numbered">
+            <li className="list-group-item d-flex justify-content-between align-items-start">
+              <div className="ms-2 me-auto">
+                <div className="fw-bold">Subheading</div>
+                Content for list item
+              </div>
+              <span className="badge text-bg-primary rounded-pill">14</span>
+            </li>
+            <li className="list-group-item d-flex justify-content-between align-items-start">
+              <div className="ms-2 me-auto">
+                <div className="fw-bold">Subheading</div>
+                Content for list item
+              </div>
+              <span className="badge text-bg-primary rounded-pill">14</span>
+            </li>
+            <li className="list-group-item d-flex justify-content-between align-items-start">
+              <div className="ms-2 me-auto">
+                <div className="fw-bold">Subheading</div>
+                Content for list item
+              </div>
+              <span className="badge text-bg-primary rounded-pill">14</span>
+            </li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
